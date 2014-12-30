de077a1646acbdd0bde97d79728ff842babaa879
julian <julian.gallegos@gmail.com>
2014-12-30 14:42:49 -0800

    added binary search algorithm to solution

--- find_ordered_set.rb
+++ find_ordered_set.rb
@@ -1,11 +1,32 @@
 # brute force solution
-def find_given_integer(integer_to_find, input_array) 
+# def find_given_integer(integer_to_find, input_array) 
 
- input_array.sort.each do |num|
- 	if num == integer_to_find
- 		return num
- 	end
- end
-end
+#  input_array.sort.each do |num|
+#  	if num == integer_to_find
+#  		return num
+#  	end
+#  end
+# end
 
-p find_given_integer(1, [4,5,6,7,8,1])
+# p find_given_integer(1, [4,5,6,7,8,1])
+
+
+def find_given_integer(integer_to_find, input_array)
+	input_array.sort!
+	low = 0
+	high = input_array.length - 1
+	while high >= low
+
+		mid_point_of_array = (high + low) / 2
+
+		if input_array[mid_point_of_array] < integer_to_find
+			low = mid_point_of_array + 1
+		elsif input_array[mid_point_of_array] > integer_to_find
+			high = mid_point_of_array - 1
+		else 
+			return input_array[mid_point_of_array]
+		end
+	end
+end	
+
+p find_given_integer(112, [0,1,2,1,4,5,2,2,4,7,8,9,10,12,14,22,112])
