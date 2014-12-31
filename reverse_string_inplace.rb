63f8567e0425995f6574c2ad433d2eaa2525805f
julian <julian.gallegos@gmail.com>
2014-12-31 00:11:33 -0800

    refactored reverse string in place method

--- reverse_string_inplace.rb
+++ reverse_string_inplace.rb
@@ -1,9 +1,27 @@
-def reverse_string_inplace(input_string)
-	left_side = 0
-	right_side = input_string.length - 1
-
-	while left_side < right_side
-		reversed_word = input_string[-1]
 
 
+def reverse_string_in_place(input_string)
+	front_of_string = 0
+	back_of_string = input_string.length - 1
+
+	while front_of_string < back_of_string
+		temp_front = input_string[front_of_string]
+		input_string[front_of_string] = input_string[back_of_string]
+		input_string[back_of_string] = temp_front
+
+		front_of_string += 1
+		back_of_string -= 1
+	end
+	return input_string
 end
+
+p reverse_string_in_place("julian") == "nailuj"
+
+p reverse_string_in_place("hello") == "olleh"
+
+p reverse_string_in_place("bye") == "eyb"
+
+
+
+
+
