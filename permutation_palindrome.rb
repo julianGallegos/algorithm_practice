2cdb367438714439c893f0e3c04b73f41102abb8
julian <julian.gallegos@gmail.com>
2014-12-30 17:01:03 -0800

    left note to try solving without recursion

--- permutation_palindrome.rb
+++ permutation_palindrome.rb
@@ -1,10 +1,29 @@
-def is_pallindrome?(input_string)
+#solved with recursion 
+
+def is_palindrome?(input_string)
 	if input_string.length <= 1
-		return "true"
+		return true
 	elsif input_string[0] != input_string[-1]
-		return "false"
-	else is_pallindrome?(input_string[1..-2])
+		return false
+	else
+		is_palindrome?(input_string[1..-2])
 	end
 end
 
-p is_pallindrome?("civic")
+
+#next....solve this without using recursion
+
+
+
+p is_palindrome?("a") == true
+
+p is_palindrome?("hi") == false
+
+p is_palindrome?("julian") == false
+
+p is_palindrome?("anna") == true
+
+p is_palindrome?("civic")
+
+
+
