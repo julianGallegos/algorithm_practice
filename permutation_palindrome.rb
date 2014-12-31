23f484c01ff48db3d42ecaab98b110276af7629c
julian <julian.gallegos@gmail.com>
2014-12-31 00:43:55 -0800

    while loop added but only compares outter characters of string but stops after first comparison

--- permutation_palindrome.rb
+++ permutation_palindrome.rb
@@ -1,20 +1,42 @@
 #solved with recursion 
 
-def is_palindrome?(input_string)
-	if input_string.length <= 1
-		return true
-	elsif input_string[0] != input_string[-1]
-		return false
-	else
-		is_palindrome?(input_string[1..-2])
-	end
-end
+# def is_palindrome?(input_string)
+# 	if input_string.length <= 1
+# 		return true
+# 	elsif input_string[0] != input_string[-1]
+# 		return false
+# 	else
+# 		is_palindrome?(input_string[1..-2])
+# 	end
+# end
 
 
 #next....solve this without using recursion
 
+def is_palindrome?(input_string)
+	front_of_string = 0
+	back_of_string = input_string.length - 1
+	
+	if input_string.length == 1
+		return true
+	end
+
+	if input_string[front_of_string] != input_string[back_of_string]
+		return false
+	end
 
 
+	while front_of_string < back_of_string
+		if input_string[front_of_string] == input_string[back_of_string]
+			front_of_string += 1
+			back_of_string -=1
+			return true
+		end
+	end
+end
+
+
+#the while loop above only seems to compare 
 
 p is_palindrome?("a") == true
 
@@ -24,7 +46,11 @@ p is_palindrome?("julian") == false
 
 p is_palindrome?("anna") == true
 
-p is_palindrome?("civic")
+p is_palindrome?("civic") == true
+
+p is_palindrome?("hannaLh") == false
+
+p is_palindrome?("hiLh") == false
 
 
 
