e6e449c33fe370dd6578ae295dba6f544c9065bb
julian <julian.gallegos@gmail.com>
2014-12-31 14:47:30 -0800

    added find word count function

--- permutation_palindrome.rb
+++ permutation_palindrome.rb
@@ -26,29 +26,34 @@ def is_palindrome?(input_string)
 	end
 
 
-	while front_of_string < back_of_string
-		if input_string[front_of_string] == input_string[back_of_string]
-			front_of_string += 1
-			back_of_string -=1
-			return true
+	if input_string[front_of_string] == input_string[back_of_string]
+		while front_of_string < back_of_string
+		front_of_string += 1
+		back_of_string -=1
+		return true
 		end
+
+		
 	end
 end
 
 
-#the while loop above only seems to compare 
+#the while loop above only seems to compare the front and back of the string 
+#the loop is already returning true even though it should be false
 
-p is_palindrome?("a") == true
+#as long as the first and last characters of the string are the same, the loop will always return true
 
-p is_palindrome?("hi") == false
+# p is_palindrome?("a") == true
 
-p is_palindrome?("julian") == false
+# p is_palindrome?("hi") == false
 
-p is_palindrome?("anna") == true
+# p is_palindrome?("julian") == false
 
-p is_palindrome?("civic") == true
+# p is_palindrome?("anna") == true
 
-p is_palindrome?("hannaLh") == false
+# p is_palindrome?("civic") == true
+
+# p is_palindrome?("hannaLh") == false
 
 p is_palindrome?("hiLh") == false
 
---
+++ word_cloud.rb
@@ -0,0 +1,20 @@
+def find_word_count(input_string)
+	
+	word_counter = Hash.new(0)
+
+	input_string.gsub!(/\W+/,' ')
+
+	p array_of_input_words = input_string.split(" ")
+
+	array_of_input_words.each do |word|
+		word_counter[word.downcase] += 1
+	end
+	return word_counter
+end
+
+def is_letter
+
+end
+
+p find_word_count("After beating the eggs, Dana read the next step:'
+'Add milk and eggs, then add flour and sugar.")
