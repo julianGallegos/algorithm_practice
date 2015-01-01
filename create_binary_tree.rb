b524972745bea811e73f6dbaac29f113d308bca2
julian <julian.gallegos@gmail.com>
2014-12-31 17:24:33 -0800

    added insert nodes into binary tree

--- create_binary_tree.rb
+++ create_binary_tree.rb
@@ -16,6 +16,22 @@ class BinaryTree
 	def initialize(root_value)
 		@root_value = TreeNode.new(root_value)
 	end
+
+	def insert_node(node_value)
+		current_node = @root_value
+		if node_value < current_node.value && current_node.left == nil
+			@root_value.left = TreeNode.new(node_value)
+		else 
+			@root_value.right = TreeNode.new(node_value)
+		end
+	end
+
 end
 
-p my_tree = BinaryTree.new(1)
+
+
+
+p my_tree = BinaryTree.new(2)
+p my_tree.insert_node(6)
+
+p my_tree
