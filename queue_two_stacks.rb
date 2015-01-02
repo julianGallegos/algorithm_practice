aa7c522ee0e6b1e33ee95475f15b0a578801dacc
julian <julian.gallegos@gmail.com>
2015-01-02 14:56:37 -0800

    added enqueue and dequeue functions

--- queue_two_stacks.rb
+++ queue_two_stacks.rb
@@ -1,26 +1,59 @@
 class Stack 
 
+	attr_accessor :store
+	
 	def initialize
 		@store = []
 	end
 
-	def push(item_to_push)
-		@store << item_to_push
+end
+
+class QueuedStack
+
+	attr_reader :inStack, :outStack
+
+	def initialize
+		@inStack = Stack.new
+		@outStack = Stack.new
 	end
 
-	def pop
-		@store.pop
+	def enqueue(item_to_push)
+		@inStack.store << item_to_push
 	end
 
-	def peek_at_next_item
-		@store.last
+	def dequeue
+		@inStack.store.length.times do
+		 @outStack.store << @inStack.store.pop
+		 end
 	end
 
-	def empty?
-		@store.empty?
+	def remove_next_in_line
+		@outStack.store.pop
 	end
+	
 
 end
+
+my_queue = QueuedStack.new
+
+my_queue.enqueue("a")
+my_queue.enqueue("b")
+my_queue.enqueue("c")
+
+my_queue.dequeue
+
+p my_queue
+
+my_queue.enqueue("d")
+
+my_queue.enqueue("e")
+
+p my_queue
+
+
+
+
+
 
 
 
