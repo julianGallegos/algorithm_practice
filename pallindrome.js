ab067108f84fa39aeccc6b2e97b07bf48ac0d2f0
julian <julian.gallegos@gmail.com>
2015-01-06 10:37:57 -0800

    added refactored solution

--- pallindrome.js
+++ pallindrome.js
@@ -1,13 +1,25 @@
 // since javascript strings are immutable, you can't do an inplace check
 
+// function isPalindrome(input_string){
+// 	if (input_string === input_string.split("").reverse().join("")){
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
+
+//refactored solution
+
 function isPalindrome(input_string){
-	if (input_string === input_string.split("").reverse().join("")){
-		return true
-	} else {
-		return false
-	}
+	return input_string === input_string.split("").reverse().join("")
 }
 
-console.log(isPalindrome("civic"))
+console.log(isPalindrome("civic") === true)
 
-console.log(isPalindrome("julian"))
+console.log(isPalindrome("level") === true)
+
+console.log(isPalindrome("levels") === false)
+
+console.log(isPalindrome("julian") === false)
+
