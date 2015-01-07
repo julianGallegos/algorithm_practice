3e66b028de1a6d5bd315f91100b6d8119680f518
julian <julian.gallegos@gmail.com>
2015-01-06 18:21:29 -0800

    added solution to find prime number from array of numbers

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -12,17 +12,37 @@ def is_a_prime_number?(num)
 		true
 end
 
-p is_a_prime_number?(29) == true
 
-p is_a_prime_number?(17) == true
+# p is_a_prime_number?(29) == true
 
-p is_a_prime_number?(13) == true
+# p is_a_prime_number?(17) == true
 
-p is_a_prime_number?(31) == true
+# p is_a_prime_number?(13) == true
 
-p is_a_prime_number?(4) == false
+# p is_a_prime_number?(31) == true
 
-p is_a_prime_number?(16) == false
+# p is_a_prime_number?(4) == false
+
+# p is_a_prime_number?(16) == false
+
+def find_prime_numbers(input_array)
+
+	prime_numbers_found = []
+
+	i = 2
+	input_array.each do |num|
+		while i < num
+			if is_a_prime_number?(num)
+				prime_numbers_found << num
+			end
+			i += 1
+		end
 
 
-# A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself
+	end
+	
+	return prime_numbers_found
+
+end
+
+p find_prime_numbers([1,2,3,4,5,6,7,8,9,10,11,12,13])
