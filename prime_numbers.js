a467a63b48ba289dbbfe34a27be2709c6ac25725
julian <julian.gallegos@gmail.com>
2015-01-07 17:20:40 -0800

    added js find prime num from array

--- prime_numbers.js
+++ prime_numbers.js
@@ -11,16 +11,29 @@ function isItPrime(num){
 }
 
 
-console.log(isItPrime(4) === false)
+// console.log(isItPrime(4) === false)
 
-console.log(isItPrime(13) === true)
+// console.log(isItPrime(13) === true)
 
-console.log(isItPrime(31) === true)
+// console.log(isItPrime(31) === true)
 
-console.log(isItPrime(1) === true)
+// console.log(isItPrime(1) === true)
 
 
+function findPrimeNumFromArray(inputArray){
 
+	var prime_numbers_found = []
+	for (var i = 0; i < inputArray.length; i++){
+			
+			if(isItPrime([inputArray[i]])){
+
+			prime_numbers_found.push(inputArray[i])
+			}
+	}
+	return prime_numbers_found
+}
+
+console.log(findPrimeNumFromArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,113]))
 
 
 
