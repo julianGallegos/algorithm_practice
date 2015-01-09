17b2469da0ca76f5b8faf7239a7a116da9756221
julian <julian.gallegos@gmail.com>
2015-01-08 16:30:58 -0800

    added more optimal solution for replacing spaces

--- replace_spaces.rb
+++ replace_spaces.rb
@@ -8,16 +8,35 @@
 
 #brute force solution
 
-def replace_blank_space(input_string)
-	#determine where the spaces are in the input string and return an index number
-	#replace that index number with "blannkspace"
-	split_string_array = input_string.split(" ")
+# def replace_blank_space(input_string)
+# 	#determine where the spaces are in the input string and return an index number
+# 	#replace that index number with "blannkspace"
+# 	split_string_array = input_string.split(" ")
 
-	split_string_array.map! do |word|
-		 word + "%20"
-	end
-	return split_string_array.join("")
+# 	split_string_array.map! do |word|
+# 		 word + "%20"
+# 	end
+# 	return split_string_array.join("")
+# end
+
+# p replace_blank_space("And I'll write")
+
+
+
+def replace_blank_space(input_string)
+		current_index = 0
+		back_of_string = input_string.length - 1
+
+		while current_index < back_of_string
+			if input_string[current_index] == " "
+				input_string[current_index] = "%20"
+			end
+				current_index += 1
+				#back_of_string needs to update to represent new size of input_string size
+				p back_of_string = input_string.length - 1
+		end
+	return input_string
 end
 
-p replace_blank_space("And I'll write")
+p replace_blank_space("a b c d e f g h i j k")
 
