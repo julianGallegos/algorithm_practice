218426952d417240ffcf932116651743c6d39057
julian <julian.gallegos@gmail.com>
2015-01-25 16:14:05 -0800

    added better solution forreversing string in place

--- reverse_string_inplace.rb
+++ reverse_string_inplace.rb
@@ -1,24 +1,38 @@
  	
 
+# def reverse_string_in_place(input_string)
+# 	front_of_string = 0
+# 	back_of_string = input_string.length - 1
+
+# 	while front_of_string < back_of_string
+# 		temp_front = input_string[front_of_string]
+# 		input_string[front_of_string] = input_string[back_of_string]
+# 		input_string[back_of_string] = temp_front
+
+# 		front_of_string += 1
+# 		back_of_string -= 1
+# 	end
+# 	return input_string
+# end
+
+# p reverse_string_in_place("julian")
+
 def reverse_string_in_place(input_string)
-	front_of_string = 0
-	back_of_string = input_string.length - 1
 
-	while front_of_string < back_of_string
-		temp_front = input_string[front_of_string]
-		input_string[front_of_string] = input_string[back_of_string]
-		input_string[back_of_string] = temp_front
+	length = input_string.length
 
-		front_of_string += 1
-		back_of_string -= 1
+	(length/2).times do |letter|
+		input_string[letter], input_string[-1-letter] = input_string[-1-letter], input_string[letter]
 	end
 	return input_string
 end
 
-p reverse_string_in_place("julian")
 
+p reverse_string_in_place('julian') == 'nailuj'
 
+p reverse_string_in_place('civic') == 'civic'
 
+p reverse_string_in_place('civics') == 'scivic'
 
 
 
