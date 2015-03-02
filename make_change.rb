44f3ec3dd82faad1ee875d45698d1db7cde44ca3
julian <julian.gallegos@gmail.com>
2015-03-02 00:06:56 -0800

    added make_change solution and test

---
+++ make_change.rb
@@ -0,0 +1,57 @@
+
+def make_change(change_amount)
+	number_of_coins = {}
+
+	
+
+	number_of_coins[:quarter] = change_amount / 25 
+
+	remaining_change = change_amount - number_of_coins[:quarter] * 25
+
+	number_of_coins[:dime] = remaining_change / 10
+
+	remaining_change = remaining_change - number_of_coins[:dime] * 10
+
+	number_of_coins[:nikels] = remaining_change / 5
+
+	remaining_change = remaining_change - number_of_coins[:nikels] * 5
+
+	number_of_coins[:pennies] = remaining_change
+
+	return number_of_coins 
+
+end
+
+
+describe "make_change" do
+
+	it "make_change(0)" do
+		expect(make_change(0)).to eq({:quarter => 0, :dime => 0, :nikels => 0, :pennies => 0})
+	end
+	
+	it "make_change(1)" do
+		expect(make_change(1)).to eq({:quarter => 0, :dime => 0, :nikels => 0, :pennies => 1})
+	end
+
+	it "make_change(99)" do
+		expect(make_change(99)).to eq({:quarter => 3, :dime => 2, :nikels => 0, :pennies => 4})
+	end
+
+	it "make_change(25)" do
+		expect(make_change(25)).to eq({:quarter => 1, :dime => 0, :nikels => 0, :pennies => 0})
+	end
+
+	it "make_change(50)" do
+		expect(make_change(50)).to eq({:quarter => 2, :dime => 0, :nikels => 0, :pennies => 0})
+	end
+
+	it "make_change(37)" do
+		expect(make_change(37)).to eq({:quarter => 1, :dime => 1, :nikels => 0, :pennies => 2})
+	end
+
+	it "make_change(41)" do
+		expect(make_change(41)).to eq({:quarter => 1, :dime => 1, :nikels => 1, :pennies => 1})
+	end
+
+
+end
